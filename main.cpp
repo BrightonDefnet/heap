c390587a88ea85ca7c447a5360ebe53ec9a1a2ad
BrightonDefnet <475373@bsd48.org>
2022-04-06 22:51:18 -0700

    reverted to last state where it makes sense

--- main.cpp
+++ main.cpp
@@ -4,23 +4,10 @@ using namespace std;
 
 int main() {
     Heap* h = new Heap();
-    int arr[100];
-    int i = 0;
-    bool running = true;
-    string entry;
 
-    while(running) {
-        cout << "enter 'num', 'file', or 'heap'" << endl;
-        cin >> entry;
-        if(entry == "num") {
-            cout << "enter a number" << endl;
-            cin >> arr[i];
-        } else if(entry == "file") {
-            cout << "getting file" << endl;
-        } else if(entry == "heap") {
-            int n = sizeof(arr) / sizeof(arr[0]);
-            h->createHeap(arr, n);
-            h->print(arr, n);
-        }
-    }
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+ 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    h->createHeap(arr, n);
+    h->print(arr, n);
 }
